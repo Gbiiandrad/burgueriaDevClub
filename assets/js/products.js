abe167fa5fb4c495390123d313fa0b5a01dc054e
Gabi Desktop <gabii12303.net@gmail.com>
2024-05-26 20:46:13 -0300

    adding to burgers list

---
+++ assets/js/products.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    {name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg'},
+    {name: 'X-Bacon', price: 30, vegan: false, src: './img/xbacon.png'},
+    {name: 'X-Bacon Egg', price: 30, vegan: false, src: './img/bacon-egg.png'},
+    {name: 'Monstruoso', price: 30, vegan: false, src: './img/monstruoso.png'},
+    {name: 'Big Vegano', price: 30, vegan: true, src: './img/monstruoso-vegan.png'},
+    {name: 'X-Vegan', price: 30, vegan: true, src: './img/xvegan.png'}
+];
