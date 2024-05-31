2ecc6d12b69e61f059d4c2e494f7b3d2a9153af7
Gabi Desktop <gabii12303.net@gmail.com>
2024-05-31 18:05:31 -0300

    funtion sumAllItems with Reduce

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 const list = document.querySelector("ul");
 const buttonShowAll = document.querySelector(".show-all");
 const buttonMapAll = document.querySelector(".map-all");
-
+const buttonSumAll = document.querySelector(".sum-all");
 
 function showAll(productsArray) {
-    // para zerar a Li sempre q for chamado
-    let myLi = "";
+  // para zerar a Li sempre q for chamado
+  let myLi = "";
 
   menuOptions.forEach((product) => {
     // para ele pegar tds e adicionar no final vamos add myLi = myLi + 1
@@ -22,19 +22,28 @@ function showAll(productsArray) {
   list.innerHTML = myLi;
 }
 
-
-
 function mapAllItems() {
   const newPrices = menuOptions.map((product) => ({
     // Spread Operator (sem precisar colocar cd campo 1 por 1 repetido. Manter os oriinais e so mudar o price)
-    
+
     ...product,
     price: product.price * 0.9, //10% de descontos
-  }))
+  }));
 
   showAll(newPrices);
+}
+
+function sumAllItems() {
+  const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0);
+  list.innerHTML = `
+        <li>
+            <p> O valor total dos itens é R$ ${totalValue}</p>
+        </li>
+        `;
+  console.log(totalValue);
 }
 
 // () => para so colocar a função quando é chamado.
 buttonShowAll.addEventListener("click", () => showAll(menuOptions));
 buttonMapAll.addEventListener("click", mapAllItems);
+buttonSumAll.addEventListener("click", sumAllItems);
