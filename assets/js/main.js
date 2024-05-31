c1428cb0fe41b285d2fb986c14e22f912cb42ad5
Gabi Desktop <gabii12303.net@gmail.com>
2024-05-31 17:55:04 -0300

    funtion newPrices with map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,40 @@
 const list = document.querySelector("ul");
-const buttonShowAll = document.querySelector('.show-all')
-let myLi = "";
+const buttonShowAll = document.querySelector(".show-all");
+const buttonMapAll = document.querySelector(".map-all");
 
-function showAll() {
-    menuOptions.forEach((product) => {
-        // para ele pegar tds e adicionar no final vamos add myLi = myLi + 1
-      myLi += `
+
+function showAll(productsArray) {
+    // para zerar a Li sempre q for chamado
+    let myLi = "";
+
+  menuOptions.forEach((product) => {
+    // para ele pegar tds e adicionar no final vamos add myLi = myLi + 1
+    myLi += `
         <li >
             <img src=${product.src}>
             <p>${product.name}</p>
             <p class="item-price">R$ ${product.price}</p>
         </li>
         `;
-    });
+  });
 
-//mostrar na tela
-list.innerHTML = myLi
+  //mostrar na tela
+  list.innerHTML = myLi;
 }
 
-buttonShowAll.addEventListener('click', showAll)
+
+
+function mapAllItems() {
+  const newPrices = menuOptions.map((product) => ({
+    // Spread Operator (sem precisar colocar cd campo 1 por 1 repetido. Manter os oriinais e so mudar o price)
+    
+    ...product,
+    price: product.price * 0.9, //10% de descontos
+  }))
+
+  showAll(newPrices);
+}
+
+// () => para so colocar a função quando é chamado.
+buttonShowAll.addEventListener("click", () => showAll(menuOptions));
+buttonMapAll.addEventListener("click", mapAllItems);
